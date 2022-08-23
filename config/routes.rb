61c0a2e6c4578c3b3ae6e716ee92bb2292f6c79a
JM Arenas <jm.arenas@interleukin.co>
2022-08-23 21:43:28 +0800

    Created routes for retrieving and updating user information

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   scope :api, defaults: { format: :json } do
     devise_for :users, controllers: { sessions: :sessions },
                        path_names: { sign_in: :login }
+    
+    resource :user, only: [:show, :update]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
