90af40371949202e43458ca950c378050fbea228
JM Arenas <jm.arenas@interleukin.co>
2022-08-23 21:26:28 +0800

    Configured Devise to allow usernames on sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,15 @@ class ApplicationController < ActionController::API
   protect_from_forgery with: :null_session
   respond_to :json
   before_action :underscore_params!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def underscore_params!
     params.deep_transform_keys!(&:underscore)
   end
+
+  private
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    end
 end
