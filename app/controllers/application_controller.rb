d2bf5aa4f674d1aca48ee6c52c5bfd8ca8d42205
JM Arenas <jm.arenas@interleukin.co>
2022-08-23 18:08:22 +0800

    Created a filter to convert incoming parameters to snake_case

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::API
   protect_from_forgery with: :null_session
+  before_action :underscore_params!
+
+  def underscore_params!
+    params.deep_transform_keys!(&:underscore)
+  end
 end
