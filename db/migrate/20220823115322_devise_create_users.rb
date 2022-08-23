030d50fab4c376cfd9584d9d51fb2e3d42434044
JM Arenas <jm.arenas@interleukin.co>
2022-08-23 19:57:14 +0800

    Added a username, image and bio column to the Users table

--- db/migrate/20220823115322_devise_create_users.rb
+++ db/migrate/20220823115322_devise_create_users.rb
@@ -3,6 +3,11 @@
 class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
+      ## Custom fields
+      t.string :username,           null: false, default: ""
+      t.string :image
+      t.string :bio
+
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -36,6 +41,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       t.timestamps null: false
     end
 
+    add_index :users, :username,             unique: true
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
