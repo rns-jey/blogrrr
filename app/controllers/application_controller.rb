47937241887a940687aeaba85d51766129d2051c
JM Arenas <jm.arenas@interleukin.co>
2022-08-23 21:34:59 +0800

    Created own authenticate_user method for authenticating users with JWTs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::API
   protect_from_forgery with: :null_session
+
   respond_to :json
+
   before_action :underscore_params!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user
 
   def underscore_params!
     params.deep_transform_keys!(&:underscore)
@@ -12,5 +15,19 @@ class ApplicationController < ActionController::API
   
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    end
+
+    def authenticate_user
+      if request.headers['Authorization'].present?
+        authenticate_or_request_with_http_token do |token|
+          begin
+            jwt_payload = JWT.decode(token, Rails.application.secrets.secret_key_base).first
+
+            @current_user_id = jwt_payload['id']
+          rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
+            head :unauthorized
+          end
+        end
+      end
     end
 end
