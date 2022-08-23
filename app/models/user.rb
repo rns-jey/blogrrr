7670bce8992a4ed4885c7becda5190cbad442a20
JM Arenas <jm.arenas@interleukin.co>
2022-08-23 20:06:01 +0800

    Added a validation to the User model for usernames

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, uniqueness: { case_sensitive: false }, presence: true, allow_blank: false, format: { with: /\A[a-zA-Z0-9]+\z/ }
          
   def generate_jwt
     JWT.encode({
